57182e212a2d59c418037497e33cfe5be9e4ce52
Kristina Popovic <3435.kristina@gmail.com>
2019-05-21 15:46:40 +0200

    load dots from file and show h2 checkbox

--- PHV.py
+++ PHV.py
@@ -8,8 +8,8 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from persistence import *
 import qdarkstyle
-
-
+import numpy as np
+import os
 class DrawingData:
 
     def __init__(self):
@@ -26,7 +26,9 @@ class DrawingData:
         # max distance for points in the 1x1 plane
         self.max_distance = 1.42
 
-    def recalculateDrawingData(self, newNumOfDots):
+        self.drawH2 = False
+
+    def recalculateDrawingData(self):
         '''
         Recalculates drawing data when the number of dots change.
         When the newNumOfDots is greater than current number of dots than the number of new random dots
@@ -37,8 +39,6 @@ class DrawingData:
         :param newNumOfDots:
         :return: None
         '''
-        self.__changeNumberOfDots(newNumOfDots)
-
         self.lines.clear()
         self.lines = calculateLines(self.dots)
 
@@ -46,7 +46,7 @@ class DrawingData:
         self.triangles = calculateTriangles(self.dots)
 
 
-    def __changeNumberOfDots(self, newNumOfDots):
+    def changeNumberOfDots(self, newNumOfDots):
         '''
         Helper method for recalculateDrawingData
         :param newNumOfDots:
@@ -63,7 +63,7 @@ class DrawingData:
 drawingData = DrawingData()
 
 
-def constructPolygons(triangles):
+def constructTriangleArray(triangles):
     '''
     Constructs polygons to be drawn by matplot
     :param triangles:
@@ -96,8 +96,11 @@ class PlotCanvas(FigureCanvas):
         :param height:
         :param dpi:
         '''
+        #matplotlib.pyplot.subplots_adjust(hspace=20)
+        
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         self.fig.set_facecolor('cornflowerblue')
+        
         self.ax = self.fig.add_subplot(121)
         self.ay = self.fig.add_subplot(122)
         FigureCanvas.__init__(self, self.fig)
@@ -117,7 +120,7 @@ class PlotCanvas(FigureCanvas):
         self.ax.set_xlim([0, 1])
         self.ax.set_ylim([0, 1])
 
-        triangleIntersectionArray = constructPolygons(drawingData.triangles)
+        triangleIntersectionArray = constructTriangleArray(drawingData.triangles)
 
         multiPoligon = cascaded_union(triangleIntersectionArray)
 
@@ -157,18 +160,22 @@ class PlotCanvas(FigureCanvas):
         self.num = 0
         self.ay.set_xlim([0,1.42])
         self.ay.axes.set_yticks([])
-        self.ay.set_yticklabels(["H0          ", "H1     "])
+        self.ay.set_yticklabels(["H0          ", "H1     ", "H2"])
         yticks = []
-        for homology in ([[(i, p) for p in dgm] for i, dgm in enumerate(drawingData.diagrams)]):
+
+        # Begin section MAGIC
+        for homology in ([[(degree, component) for component in dgm] for (degree, dgm) in enumerate(drawingData.diagrams)]):
             self.ay.plot([0, 1.42], [self.num, self.num], "black")
             yticks.append(self.num)
             self.ay.axes.set_yticks(yticks)
-            homology = filter(lambda x: x[0] != 2, homology)
-            for(_, b) in homology:
-                self.ay.fill([b.birth, b.birth, min(b.death, drawingData.max_distance), min(b.death, drawingData.max_distance)], [
+            if not drawingData.drawH2:
+                homology = filter(lambda hom: hom[0] < 2, homology)
+
+            for (_, component) in homology:
+                self.ay.fill([component.birth, component.birth, min(component.death, drawingData.max_distance), min(component.death, drawingData.max_distance)], [
                             self.num, self.num+1, self.num+1, self.num], alpha=0.9)
                 self.num = self.num + 1
-
+        # End section MAGIC
         lines = self.ay.plot([drawingData.epsilon, drawingData.epsilon], [0, self.num], "black")
 
         self.epsilon_line = lines.pop()
@@ -229,7 +236,9 @@ class Window(QWidget):
         '''
         self.textBoxNumOfDots.setText(str(num))
 
-        drawingData.recalculateDrawingData(num)
+        drawingData.changeNumberOfDots(num)
+
+        drawingData.recalculateDrawingData()
 
         vrc = VietorisRipsComplex(
             list_of_pairs_to_numpyarray(drawingData.dots))
@@ -264,7 +273,7 @@ class Window(QWidget):
         '''
         try:
             a = float(string)
-            if(a >= 0 and a <= 30):
+            if(a >= 0 and a <= 16):
                 self.sliderNumDots.setValue((a))
         except:
             return
@@ -281,6 +290,31 @@ class Window(QWidget):
         else:
             app.setStyleSheet('')
 
+    def onLoadFileButtonClick(self):
+        try:
+            file = QFileDialog.getOpenFileName(self, 'Open file', os.curdir," (*.csv )")
+            drawingData.dots = np.loadtxt(fname = file[0], delimiter=',', dtype=np.float)
+            drawingData.recalculateDrawingData()
+            
+            vrc = VietorisRipsComplex(drawingData.dots)
+            
+            drawingData.diagrams = vrc.compute_persistence(drawingData.max_distance)
+
+            self.canvas.setDrawingData(drawingData)
+
+            self.canvas.barCode()
+
+            self.canvas.plot()
+            # gui postavi broj tacaka, slider ...
+        except IOError:
+            pass
+
+
+    def changeH2(self):
+        drawingData.drawH2 = self.H2CheckBox.isChecked()
+
+        self.canvas.barCode()
+
     def __init__(self, parent=None):
         '''
 
@@ -295,7 +329,7 @@ class Window(QWidget):
         self.sliderNumDots = QSlider(Qt.Horizontal)
         self.sliderEpsilon = QSlider(Qt.Horizontal)
 
-        self.sliderNumDots.setMaximum(30)
+        self.sliderNumDots.setMaximum(16)
         self.sliderNumDots.setMinimum(0)
 
         self.sliderEpsilon.setMaximum(1420)
@@ -313,7 +347,11 @@ class Window(QWidget):
         self.textBoxNumOfDots.setMaxLength(3)
         self.textBoxEpsilon.setMaxLength(4)
 
-        self.CheckBox = QCheckBox("Pređi na \ntamnu stranu")
+        self.DarkThemeCheckBox = QCheckBox("Pređi na \ntamnu stranu")
+        self.H2CheckBox = QCheckBox("Prikaži H2")
+
+        self.Button = QPushButton("Izaberi fajl sa tackama")
+        self.Button.clicked.connect(self.onLoadFileButtonClick)
 
         self.canvas = PlotCanvas(self, width=1, height=1)
         self.canvas.setDrawingData(drawingData)
@@ -327,24 +365,27 @@ class Window(QWidget):
         self.textBoxEpsilon.textEdited.connect(self.onTextBoxEpsilonChanged)
         self.textBoxNumOfDots.textEdited.connect(self.onTextBoxNumOfDotsChanged)
 
-        self.CheckBox.stateChanged.connect(self.changeTheme)
+        self.DarkThemeCheckBox.stateChanged.connect(self.changeTheme)
+        self.H2CheckBox.stateChanged.connect(self.changeH2)
         self.Labela = QLabel("Kristina Popović & Marko Spasić, 2019")
         grid.setContentsMargins(8, 8, 8, 8)
 
         grid.addWidget(labelaNumDots, 0, 0)
         grid.addWidget(self.sliderNumDots, 0, 1)
         grid.addWidget(self.textBoxNumOfDots, 0, 2)
+        grid.addWidget(self.Button, 0,3)
+        grid.addWidget(self.H2CheckBox,1,3)
         grid.addWidget(labelaEpsilon, 1, 0)
         grid.addWidget(self.sliderEpsilon, 1, 1)
         grid.addWidget(self.textBoxEpsilon, 1, 2)
-        grid.addWidget(self.canvas, 3, 0, 1, 3)
-        grid.addWidget(self.CheckBox, 4, 2)
+        grid.addWidget(self.canvas, 3, 0, 1, 4)
+        grid.addWidget(self.DarkThemeCheckBox, 4, 3)
         grid.addWidget(self.Labela,4,0)
 
         #grid.addWidget(self.createSlider("Random faktor"), 2, 0)
         self.setLayout(grid)
         self.setWindowTitle("Perzistentna homologija")
-        self.resize(800, 800)
+        self.resize(1490, 960)
 
 
 def main():
@@ -353,6 +394,7 @@ def main():
     app.setStyle("Fusion")
     clock = Window()
     clock.show()
+    #clock.showFullScreen()
     sys.exit(app.exec_())
 
 
